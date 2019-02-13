44ce1591414ea154783eec40c8daee9cfe570630
Distribuidos <miguel11andres@hotmail.com>
2019-02-13 18:49:08 -0500

    Cambio cookbook_file

--- cookbooks/httpd/recipes/httpd_copy_files.rb
+++ cookbooks/httpd/recipes/httpd_copy_files.rb
@@ -1,7 +1,12 @@
-template 'var/www/html/index.html' do
-  source 'index.erb'
-  mode  0644
-  variables(
-    :name => node[:name]
-  )
+#template 'var/www/html/index.html' do
+#  source 'index.erb'
+#  mode  0644
+#  variables(
+#    :name => node[:name]
+#  )
+#end
+
+cookbook_file '/var/www/html/icesi.html' do
+  source 'index.html'
+  mode 0644
 end

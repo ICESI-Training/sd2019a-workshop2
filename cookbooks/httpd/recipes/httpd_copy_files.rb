f347e50639b8aab75789b7b4edc4e8cc2d4358a7
ikermatias <josegar0218@gmail.com>
2019-02-13 18:49:17 -0500

    Update httpd_copy_files

--- cookbooks/httpd/recipes/httpd_copy_files.rb
+++ cookbooks/httpd/recipes/httpd_copy_files.rb
@@ -1,7 +1,12 @@
-template '/var/www/html/index.html' do
-  source 'index.erb'
-  mode 0644
-  variables(
-    :name => [:name]
-  )
+#template '/var/www/html/index.html' do
+#  source 'index.erb'
+#  mode 0644
+#  variables(
+#    :name => [:name]
+#  )
+#end
+cookbook_file '/var/www/html/index.html' do
+	source 'index.html'
+	mode 0644
 end
+

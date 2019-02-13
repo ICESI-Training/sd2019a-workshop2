83201d406954b62769d285782c7d044da978c97a
Marisol Giraldo <marisol.giraldo@correo.icesi.edu.co>
2019-02-13 18:49:17 -0500

    Update files

--- cookbooks/httpd/recipes/httpd_copy_files.rb
+++ cookbooks/httpd/recipes/httpd_copy_files.rb
@@ -1,8 +1,13 @@
-template '/var/www/html/index.html' do
-	source 'index.erb'
-	mode 0644
-	variables(
-		:name => node[:name]
-	)
+#template '/var/www/html/index.html' do
+#	source 'index.erb'
+#	mode 0644
+#	variables(
+#		:name => node[:name]
+#	)
 end
-	
+
+
+cookbook_file 'var/www/html/index.html' do
+	souce 'index.html'
+	mode 0644
+end

ebadf83e83609d60a5d951dc2beb99871d66a67a
danielperga97 <danielperga97@outlook.com>
2019-02-13 18:49:30 -0500

    cookbook_file update

--- cookbooks/httpd/recipes/httpd_copy_files.rb
+++ cookbooks/httpd/recipes/httpd_copy_files.rb
@@ -1,7 +1,12 @@
-template '/var/www/html/index.html' do
-	source 'index.erb'
+#template '/var/www/html/icesi.html' do
+#	source 'icesi.erb'
+#	mode 0644
+#	variables(
+#		:name => node[:name]
+#	)
+#end
+
+cookbook_file '/var/www/html/icesi.html' do
+	source 'index.html'
 	mode 0644
-	variables(
-		:name => node[:name]
-	)
 end

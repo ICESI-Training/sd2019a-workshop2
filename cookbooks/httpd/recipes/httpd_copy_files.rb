391c2625b8f8d038b4fb433944d041585c1223a6
StevenMontealegre <james.montealegre@correo.icesi.edu.co>
2019-02-13 18:50:41 -0500

    change httpd_copy_files.rb

--- cookbooks/httpd/recipes/httpd_copy_files.rb
+++ cookbooks/httpd/recipes/httpd_copy_files.rb
@@ -1,9 +1,13 @@
-template 'var/www/html/index.html' do
-  source 'index.erb'
-  mode  0644
-  variables(
-    :name => node[:name]
-  )
-end
+#template 'var/www/html/index.html' do
+#  source 'index.erb'
+#  mode  0644
+#  variables(
+#    :name => node[:name]
+#  )
+#end
 
+cookbook_file '/var/www/html/index.html' do
+	source 'index.html'
+	mode 0644
+end
 
